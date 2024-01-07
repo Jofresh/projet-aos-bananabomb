7510ae60ad5457d39934360d74e85eda2827b742
Jofresh <gaborit.geoffray@gmail.com>
2024-01-07 18:02:05 +0100

    wip: connection between front and back + docker

--- back/src/index.ts
+++ back/src/index.ts
@@ -10,41 +10,16 @@ import { UnknownRoutesHandler } from '~/middlewares/unknownRoutes.handler'
  */
 const app = express()
 
-/**
- * On dit à Express que l'on souhaite parser le body des requêtes en JSON
- *
- * @example app.post('/', (req) => req.body.prop)
- */
 app.use(express.json())
 
-/**
- * On dit à Express que l'on souhaite autoriser tous les noms de domaines
- * à faire des requêtes sur notre API.
- */
 app.use(cors())
 
-/**
- * Toutes les routes CRUD pour les animaux seronts préfixées par `/pets`
- */
 app.use('/', BotsController)
 
-/**
- * Homepage (uniquement necessaire pour cette demo)
- */
 app.get('/', (req, res) => res.send('🏠'))
 
-/**
- * Pour toutes les autres routes non définies, on retourne une erreur
- */
 app.all('*', UnknownRoutesHandler)
 
-/**
- * Gestion des erreurs
- * /!\ Cela doit être le dernier `app.use`
- */
 app.use(ExceptionsHandler)
 
-/**
- * On demande à Express d'ecouter les requêtes sur le port défini dans la config
- */
 app.listen(config.API_PORT, () => console.log('Silence, ça tourne.'))
