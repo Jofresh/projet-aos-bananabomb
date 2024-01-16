540f2c15b592553182cc7cd6153917d66962e38e
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2024-01-16 13:00:38 +0100

    feat: learning rate working (see stashed refacto)

--- back/src/resources/ai_learning.service.ts
+++ back/src/resources/ai_learning.service.ts
@@ -1,140 +1,139 @@
-import fs from 'fs';
-import path from 'path';
-import { FILE_DB_PATH } from '~/config';
-import { bestPath, gameTile } from '~~/types/ai';
-import { DIRECTION } from '~~/types/game';
+import fs from 'fs'
+import path from 'path'
+import { FILE_DB_PATH } from '~/config'
+import { bestPath, gameTile } from '~~/types/ai'
+import { DIRECTION } from '~~/types/game'
 
 class AILearning {
-    constructor() {}
-
-    formatMap(map: gameTile[]) {
-        try {
-            const botPosition = map.filter(caseMap => caseMap.value === 1)[0];
-            return map.filter(caseMap => (caseMap.x <= botPosition.x + 3) && (caseMap.y <= botPosition.y + 3) && (caseMap.x >= botPosition.x - 3) && (caseMap.y >= botPosition.y - 3))
-        } catch (e) {
-            console.log(e);
-        }
-
-        return [];
+  formatMap(map: gameTile[]) {
+    try {
+      const botPosition = map.filter(caseMap => caseMap.value === 1)[0]
+      return map.filter(caseMap => (caseMap.x <= botPosition.x + 3) && (caseMap.y <= botPosition.y + 3) && (caseMap.x >= botPosition.x - 3) && (caseMap.y >= botPosition.y - 3))
+    } catch (e) {
+      console.log(e)
     }
-    
-    newLearning(givenMap: gameTile[]) {
-        const possibleDirections = Object.values(DIRECTION);
-            
-        let bestPathValue = 0;
-        let bestPath: bestPath = { firstStep: "N", secondStep: "N", thirdStep: "N" };
 
-        try {
-            for (let k = 1; k < 1000; k++) {
-                let parsedMap: gameTile[] = JSON.parse(JSON.stringify(givenMap));
-                let botPosition = parsedMap.find(coords => coords.value === 1) || { x: 0, y: 0, value: 0 };
-                
-                let currentPathValue = 0;
-                let pathValue = "";
-                for (let j = 0; j < 3; j++) { 
-                    let newPosBot: gameTile = { 
-                        x: botPosition.x,
-                        y: botPosition.y,
-                        value: botPosition.value
-                    };
+    return []
+  }
 
-                    const randomDirection = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
-                    pathValue += randomDirection.charAt(0);
-                    switch (randomDirection) {
-                        case DIRECTION.LEFT:
-                            // TODO : typo
-                            const canMove = parsedMap.filter(coords => (coords.x == newPosBot.x - 1) && (coords.y == newPosBot.y) && (coords.value != 0)).length > 0;
-                            if (newPosBot.x > 0 && canMove) {
-                                newPosBot.x = newPosBot.x - 1;
-                                let tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0];
-                                newPosBot.value = tmpValue.value;
-                                currentPathValue = currentPathValue + newPosBot.value;
-                            }
-                            break;
-                        case DIRECTION.UP:
-                            if(newPosBot.y > 0 && parsedMap.filter(coords => (coords.x == newPosBot.x) && (coords.y == newPosBot.y - 1) && (coords.value != 0))){
-                                newPosBot.y = newPosBot.y - 1;
-                                let tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0];
-                                newPosBot.value = tmpValue.value;
-                                currentPathValue = currentPathValue + newPosBot.value;
-                            }
-                            break;
-                        case DIRECTION.DOWN:
-                            if(parsedMap.filter(coords => coords.y == newPosBot.y + 1).length && parsedMap.filter(coords => (coords.x == newPosBot.x) && (coords.y == newPosBot.y + 1) && (coords.value != 0))){
-                                newPosBot.y = newPosBot.y + 1;
-                                let tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0];
-                                newPosBot.value = tmpValue.value;
-                                currentPathValue = currentPathValue + newPosBot.value;
-                            }
-                            break;
-                        case DIRECTION.RIGHT:
-                            if(parsedMap.filter(coords => coords.x == newPosBot.x + 1).length && parsedMap.filter(coords => (coords.x == newPosBot.x + 1) && (coords.y == newPosBot.y) && (coords.value != 0))){
-                                newPosBot.x = newPosBot.x + 1;
-                                let tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0];
-                                newPosBot.value = tmpValue.value;
-                                currentPathValue = currentPathValue + newPosBot.value;
-                            }
-                            break;
-                        default:
-                    }
+  newLearning(givenMap: gameTile[]) {
+    const possibleDirections = Object.values(DIRECTION)
 
-                    parsedMap.filter(coords => coords.x == botPosition.x && coords.y == botPosition.y).forEach(coords => {
-                        coords.value = 3
-                    });
+    let bestPathValue = 0
+    let bestPath: bestPath = { firstStep: 'N', secondStep: 'N', thirdStep: 'N' }
 
-                    botPosition = {
-                        x: newPosBot.x,
-                        y: newPosBot.y,
-                        value: 1
-                    };
-                }
+    try {
+      for (let k = 1; k < 1000; k++) {
+        const parsedMap: gameTile[] = JSON.parse(JSON.stringify(givenMap))
+        let botPosition = parsedMap.find(coords => coords.value === 1) || { x: 0, y: 0, value: 0 }
 
-                if (currentPathValue > bestPathValue) {
-                    bestPathValue = currentPathValue;
-                    bestPath = {
-                        firstStep: pathValue.charAt(0),
-                        secondStep: pathValue.charAt(1),
-                        thirdStep: pathValue.charAt(2)
-                    };
-                }
+        let currentPathValue = 0
+        let pathValue = ''
+        for (let j = 0; j < 3; j++) {
+          const newPosBot: gameTile = {
+            x: botPosition.x,
+            y: botPosition.y,
+            value: botPosition.value
+          }
+
+          const randomDirection = possibleDirections[Math.floor(Math.random() * possibleDirections.length)]
+          pathValue += randomDirection.charAt(0)
+          switch (randomDirection) {
+          case DIRECTION.LEFT:
+            // TODO : typo
+            // eslint-disable-next-line no-case-declarations
+            const canMove = parsedMap.filter(coords => (coords.x == newPosBot.x - 1) && (coords.y == newPosBot.y) && (coords.value != 0)).length > 0
+            if (newPosBot.x > 0 && canMove) {
+              newPosBot.x = newPosBot.x - 1
+              const tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0]
+              newPosBot.value = tmpValue.value
+              currentPathValue = currentPathValue + newPosBot.value
             }
+            break
+          case DIRECTION.UP:
+            if(newPosBot.y > 0 && parsedMap.filter(coords => (coords.x == newPosBot.x) && (coords.y == newPosBot.y - 1) && (coords.value != 0))){
+              newPosBot.y = newPosBot.y - 1
+              const tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0]
+              newPosBot.value = tmpValue.value
+              currentPathValue = currentPathValue + newPosBot.value
+            }
+            break
+          case DIRECTION.DOWN:
+            if(parsedMap.filter(coords => coords.y == newPosBot.y + 1).length && parsedMap.filter(coords => (coords.x == newPosBot.x) && (coords.y == newPosBot.y + 1) && (coords.value != 0))){
+              newPosBot.y = newPosBot.y + 1
+              const tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0]
+              newPosBot.value = tmpValue.value
+              currentPathValue = currentPathValue + newPosBot.value
+            }
+            break
+          case DIRECTION.RIGHT:
+            if(parsedMap.filter(coords => coords.x == newPosBot.x + 1).length && parsedMap.filter(coords => (coords.x == newPosBot.x + 1) && (coords.y == newPosBot.y) && (coords.value != 0))){
+              newPosBot.x = newPosBot.x + 1
+              const tmpValue = parsedMap.filter(coords => coords.x == newPosBot.x && coords.y == newPosBot.y )[0]
+              newPosBot.value = tmpValue.value
+              currentPathValue = currentPathValue + newPosBot.value
+            }
+            break
+          default:
+          }
 
-            const idMap = this.generateIdMap(givenMap);
-            this.writeInFile(idMap, bestPath);
-        } catch (e) {
-            console.log(e);
+          parsedMap.filter(coords => coords.x == botPosition.x && coords.y == botPosition.y).forEach(coords => {
+            coords.value = 3
+          })
+
+          botPosition = {
+            x: newPosBot.x,
+            y: newPosBot.y,
+            value: 1
+          }
         }
 
-        return "Working fine";
-    }
-
-    writeInFile(id: string, result: bestPath) {
-        try {
-            const computedPath = result.firstStep + result.secondStep + result.thirdStep;
-            
-            const file = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8');
-            const parsedFile = JSON.parse(file)
-            
-            const fileContent = {
-                ...parsedFile,
-                [id]: computedPath
-            };
-
-            fs.writeFileSync(path.join(__dirname, FILE_DB_PATH), JSON.stringify(fileContent))
-        } catch (e) {
-            console.log(e);
+        if (currentPathValue > bestPathValue) {
+          bestPathValue = currentPathValue
+          bestPath = {
+            firstStep: pathValue.charAt(0),
+            secondStep: pathValue.charAt(1),
+            thirdStep: pathValue.charAt(2)
+          }
         }
+      }
+
+      const idMap = this.generateIdMap(givenMap)
+      this.writeInFile(idMap, bestPath)
+    } catch (e) {
+      console.log(e)
     }
 
-    generateIdMap(map: gameTile[]) {
-        return map.reduce((acc, gameTile) => acc + (gameTile.value || 0), "");
-    }
+    return 'Working fine'
+  }
 
-    readFile() {
-        const file = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8');
-        const parsedFile = JSON.parse(file)
-        return Object.keys(parsedFile).length || 0;
+  writeInFile(id: string, result: bestPath) {
+    try {
+      const computedPath = result.firstStep + result.secondStep + result.thirdStep
+
+      const file = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8')
+      const parsedFile = JSON.parse(file)
+
+      const fileContent = {
+        ...parsedFile,
+        [id]: computedPath
+      }
+
+      fs.writeFileSync(path.join(__dirname, FILE_DB_PATH), JSON.stringify(fileContent))
+    } catch (e) {
+      console.log(e)
     }
+  }
+
+  generateIdMap(map: gameTile[]) {
+    return map.reduce((acc, gameTile) => acc + (gameTile.value || 0), '')
+  }
+
+  readFile() {
+    const file = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8')
+    const parsedFile = JSON.parse(file)
+    return Object.keys(parsedFile).length || 0
+  }
 }
 
-export default new AILearning();
+export default new AILearning()
--- back/src/resources/bots.controller.ts
+++ back/src/resources/bots.controller.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import fs from 'fs';
-import path from 'path';
+import { Router } from 'express'
+import fs from 'fs'
+import path from 'path'
 
-import { FILE_DB_PATH } from '~/config';
-import { DIFFICULTY, DIRECTION } from '~~/types/game';
-import AILearningInstance from './ai_learning.service';
+import { FILE_DB_PATH } from '~/config'
+import { DIFFICULTY, DIRECTION } from '~~/types/game'
+import AILearningInstance from './ai_learning.service'
 
 let jsonStateBeforeGame = 0
 let jsonStateAfterGame = 0
@@ -13,61 +13,58 @@ const BotsController = Router()
 
 // This endpoint is called by the front to get the next moves of the bot
 BotsController.post('/bot-movement', (req, res) => {
-   const formattedMap = AILearningInstance.formatMap(req.body.map || []);
-   const mapId = AILearningInstance.generateIdMap(formattedMap);
+  const formattedMap = AILearningInstance.formatMap(req.body.map || [])
+  const mapId = AILearningInstance.generateIdMap(formattedMap)
 
-   const dbFile = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8');
-   const parsedFile = JSON.parse(dbFile);
-   if (!!parsedFile[mapId]) {
-      return res.status(200).json({ nextMoves: parsedFile[mapId] })
-   }
+  const dbFile = fs.readFileSync(path.join(__dirname, FILE_DB_PATH), 'utf-8')
+  const parsedFile = JSON.parse(dbFile)
+  if (parsedFile[mapId]) {
+    return res.status(200).json({ nextMoves: parsedFile[mapId] })
+  }
 
-   AILearningInstance.newLearning(formattedMap);
+  AILearningInstance.newLearning(formattedMap)
 
-   // Picks three random moves
-   const ACTIONS = Object.values(DIRECTION);
-   const nextMoves = Array.from({ length: 3 }, () => ACTIONS[Math.floor(Math.random() * ACTIONS.length)]).join('');
-   return res.status(200).json({ nextMoves });
+  // Picks three random moves
+  const ACTIONS = Object.values(DIRECTION)
+  const nextMoves = Array.from({ length: 3 }, () => ACTIONS[Math.floor(Math.random() * ACTIONS.length)]).join('')
+  return res.status(200).json({ nextMoves })
 })
 
 // This endpoint is called by the front to initialize the game
 BotsController.post('/game', (req, res) => {
-   jsonStateBeforeGame = AILearningInstance.readFile()
+  jsonStateBeforeGame = AILearningInstance.readFile()
 
-   const difficulty = req.body.difficulty;
+  const difficulty = req.body.difficulty
 
-   if (!!difficulty && Object.values(DIFFICULTY).includes(difficulty)) {
-      // ... should set the difficulty here
-      return res.status(200).json("Succès dans la définition de la difficulté.")
-   }
+  if (!!difficulty && Object.values(DIFFICULTY).includes(difficulty)) {
+    // ... should set the difficulty here
+    return res.status(200).json('Succès dans la définition de la difficulté.')
+  }
 
-   // Picks a random difficulty
-   const difficulties = Object.values(DIFFICULTY);
-   const randomDifficulty = difficulties[Math.floor(Math.random() * difficulties.length)];
+  // Picks a random difficulty
+  const difficulties = Object.values(DIFFICULTY)
+  const randomDifficulty = difficulties[Math.floor(Math.random() * difficulties.length)]
 
-   // ... should set the difficulty here
-   return res.status(200).json("Succès dans la définition de la difficulté.")
+  // ... should set the difficulty here
+  return res.status(200).json('Succès dans la définition de la difficulté.')
 })
 
 // This endpoint is called by the front to stop the game
 BotsController.delete('/game', (req, res) => {
-   jsonStateAfterGame = AILearningInstance.readFile()
-   try{
-      return res.status(200).json("Succès dans la suppression de la partie.")
-   } catch(NotFoundException) {
-      return res.json("Erreur dans la suppression de la partie.")
-   }
+  jsonStateAfterGame = AILearningInstance.readFile()
+  try{
+    return res.status(200).json('Succès dans la suppression de la partie.')
+  } catch(NotFoundException) {
+    return res.json('Erreur dans la suppression de la partie.')
+  }
 })
 
 // This endpoint is called by the front to get the learning rate of the bot
 BotsController.get('/learning-rate', (req, res) => {
-   const newResult = jsonStateAfterGame - jsonStateBeforeGame
-   const percentageResult =  100 * (newResult / jsonStateAfterGame) 
-   try{
-      return res.status(200).json("Chemin apprit par l'IA :" + " " + newResult + " " + "/" + " " + "Voici le taux d'apprentissage de l'IA sur cette partie : " + percentageResult + "%")
-   } catch(NotFoundException){
-      return res.json("Erreur dans la récupération du résultat de l'apprentissage de l'IA.")
-   }
+  const newResult = jsonStateAfterGame - jsonStateBeforeGame
+  const percentageResult =  100 * (newResult / jsonStateAfterGame)
+
+  return res.status(200).json({ learnedPathsNumber: newResult, percentageResult })
 })
 
 export { BotsController }
